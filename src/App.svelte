2e8abe437a46b072510e33d659fe5893c5dd7e32
Matt Riley <matt@mattriley.me>
2020-02-11 22:12:59 +0000

    Initial commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,43 @@
 <script>
-	export let name;
 </script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
 		max-width: 240px;
+		height: 100%;
 		margin: 0 auto;
+		display:flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: #2836fc;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+		font-family: 'Rubik Mono One', sans-serif;
 	}
 
+	.attribute-footer {
+		margin-top: 5em;
+		font-size: .6em;
+	}
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
 		}
 	}
 </style>
+
+<svelte:head>
+	<title>ALLCAPS.DEV</title>
+	<link href="https://fonts.googleapis.com/css?family=Rubik+Mono+One&display=swap" rel="stylesheet">
+</svelte:head>
+
+<main>
+	<h1>ALLCAPS.DEV</h1>
+	<span class="icons"><a href="https://github.com/ALLCAPSDEV"><img src="/icons8-github-50.svg" alt="Github"></a> <a href="https://www.npmjs.com/org/allcapsdev"><img src="/icons8-npm-50.svg" alt="NPMJS"></a></span>
+	<span class="attribute-footer"><a target="_blank" href="https://icons8.com/icons/set/github--v1">GitHub</a>, <a target="_blank" href="https://icons8.com/icons/set/npm">NPM</a> and other icons by <a target="_blank" href="https://icons8.com">Icons8</a></span>
+</main>
