d09eb54eb4693d29128f209e1caaf6db5f0fa739
Matt Riley <matt@mattriley.me>
2020-04-28 20:24:14 +0100

    Added automatic darkmode

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,28 @@
 <script>
 	import Logo from './logo.svelte';
+	if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+		console.log(window.matchMedia('(prefers-color-scheme: dark').matches)
+		window.document.body.classList.toggle('dark-mode')
+	}
+	window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+    window.document.body.classList.toggle('dark-mode')
+	});
 </script>
 
 <style>
+	:global(body) {
+		background-color: #f2eee2;
+		color: #3f3f3f;
+		transition: background-color 0.3s
+	}
+	:global(body.dark-mode) {
+		background-color: #3f3f3f;
+		color: #f2eee2;
+	}
 	main {
 		width: 240px;
 		margin: 0 auto;
-		max-height: 99vh;
+		height: 99vh;
 		display:grid;
 		grid-template-columns: 1fr;
 		grid-template-rows: repeat(4, 1fr);
@@ -47,8 +63,9 @@
 	<link href="https://fonts.googleapis.com/css?family=Rubik+Mono+One&display=swap" rel="stylesheet">
 </svelte:head>
 
-<main>
-	<Logo />
-	<span class="icons"><a href="https://github.com/ALLCAPSDEV"><img src="/icons8-github-50.svg" alt="Github"></a> <a href="https://www.npmjs.com/org/allcapsdev"><img src="/icons8-npm-50.svg" alt="NPMJS"></a></span>
-	<span class="attribute-footer"><a target="_blank" href="https://icons8.com/icons/set/github--v1">GitHub</a>, <a target="_blank" href="https://icons8.com/icons/set/npm">NPM</a> and other icons by <a target="_blank" href="https://icons8.com">Icons8</a></span>
-</main>
+	<main>
+		<Logo />
+		<span class="icons"><a href="https://github.com/ALLCAPSDEV"><img src="/icons8-github-50.svg" alt="Github"></a> <a href="https://www.npmjs.com/org/allcapsdev"><img src="/icons8-npm-50.svg" alt="NPMJS"></a></span>
+		<span class="attribute-footer"><a target="_blank" href="https://icons8.com/icons/set/github--v1">GitHub</a>, <a target="_blank" href="https://icons8.com/icons/set/npm">NPM</a> and other icons by <a target="_blank" href="https://icons8.com">Icons8</a></span>
+	</main>
+
