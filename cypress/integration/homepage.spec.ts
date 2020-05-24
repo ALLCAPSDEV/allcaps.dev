a29fad88ed9ee9f112db8f7f2d08bf7a13ffbab2
Matt Riley <matt@mattriley.me>
2020-05-24 15:50:43 +0100

    Two further tests

--- cypress/integration/homepage.spec.ts
+++ cypress/integration/homepage.spec.ts
@@ -1,6 +1,16 @@
 describe('Homepage', () => {
-  it('Displays the logo', () => {
+  beforeEach(() => {
     cy.visit('/');
+  })
+  it('Displays the logo', () => {
     cy.get('[data-cy=main-logo]').should("be.visible");
   })
+  it('Displays the npm icon', () => {
+    cy.get('[data-cy=npm-icon]').should('be.visible')
+  })
+
+  it('Displays the Github icon', () => {
+    cy.get('[data-cy=github-icon]').should('be.visible')
+  })
+
 })
