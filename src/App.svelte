f9df20f1a2f8b9f20d4514cb7c6713c7a4fe5d09
Matt Riley <matt@mattriley.me>
2020-06-26 17:45:28 +0100

    Initial commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,57 +1,52 @@
 <script>
-	import Logo from './logo.svelte';
+  import Header from './components/Header.svelte'
 </script>
 
 <style>
-	:global(body) {
-		background-color: #f2eee2;
-		color: #3f3f3f;
-		transition: background-color 0.3s
-	}
+  :global(body) {
+    background-color: #f2eee2;
+    color: #3f3f3f;
+    transition: background-color 0.3s;
+  }
 
-	@media (prefers-color-scheme: dark) {
-		:global(body) {
-			background-color: #3f3f3f;
-			color: #f2eee2;
-		}
-	}
-	main {
-		width: 240px;
-		margin: 0 auto;
-		height: 99vh;
-		display:grid;
-		grid-template-columns: 1fr;
-		grid-template-rows: repeat(4, 1fr);
-		justify-content: center;
-		align-content: center;
-		align-items: center;
-	}
+  @media (prefers-color-scheme: dark) {
+    :global(body) {
+      background-color: #3f3f3f;
+      color: #f2eee2;
+    }
+  }
+  main {
+    width: 95%;
+    margin: 0 auto;
+    height: 99vh;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(3, auto);
+  }
 
-	.icons {
-		grid-row-start: 3;
-		justify-self: center;
-	}
+  .icons {
+    grid-row-start: 3;
+    justify-self: center;
+  }
 
-	.attribute-footer {
-		margin-top: 5em;
-		font-size: .6em;
-		align-self: flex-end;
-		grid-row-start: 4;
-		justify-self: center;
-	}
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .attribute-footer {
+    margin-top: 5em;
+    font-size: 0.6em;
+    align-self: flex-end;
+    grid-row-start: 4;
+    justify-self: center;
+  }
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
 
 <svelte:head>
-	<title>ALLCAPS.DEV</title>
+  <title>ALLCAPS.DEV</title>
 </svelte:head>
 
 <main>
-	<Logo />
-	<span class="icons"><a href="https://github.com/ALLCAPSDEV"><img src="/icons8-github-50.svg" alt="Github" data-cy="github-icon"></a> <a href="https://www.npmjs.com/org/allcapsdev"><img src="/icons8-npm-50.svg" alt="NPM JS" data-cy="npm-icon"></a></span>
-	<span class="attribute-footer"><a target="_blank" href="https://icons8.com/icons/set/github--v1">GitHub</a>, <a target="_blank" href="https://icons8.com/icons/set/npm">NPM</a> and other icons by <a target="_blank" href="https://icons8.com">Icons8</a></span>
+  <Header />
 </main>
---
+++ src/components/Header.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Logo from './Logo.svelte'
+</script>
+
+<style>
+  header {
+    display: grid;
+    grid-template-columns: 10vw 1fr;
+    margin: 0;
+    padding: 0;
+    max-height: 8vh;
+    overflow: hidden;
+    width: 100%;
+    margin-top: 2.5vw;
+  }
+  nav {
+    width: 100%;
+  }
+</style>
+
+<header>
+  <Logo />
+  <nav>
+    <h1>Hey</h1>
+    <h1>Ho</h1>
+  </nav>
+</header>
